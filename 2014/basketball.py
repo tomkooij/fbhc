14e53b6217b80aa26823226d3bf7926dac8ded88
tomkooij <tomkooij@tomkooij.nl>
2016-01-07 17:38:00 +0100

    2014: q2: basketball finised

--- 2014/basketball.py
+++ 2014/basketball.py
@@ -6,24 +6,55 @@ TESTCASE = 'input/basketball_game_example_input.txt'
 INPUT = 'input/basketball_game.txt'
 
 playerconv = lambda x : (x[0], int(x[1]), int(x[2]))
+addtime = lambda x : (x[0], x[1]+1, x[2])
 
 if __name__ == '__main__':
-    with open(TESTCASE) as f:
+    with open(INPUT) as f:
         T = int(f.readline())
 
 
         for case in range(1,T+1):
-            N,M,P =  map(int,f.readline().split())
-            print "DBG, case",(T,N,M,P)
+            N, M, P =  map(int,f.readline().split())
+            #print "DBG, case",(case,N,M,P)
 
             players = []
             for _ in range(0,N):
                 players.append(playerconv(f.readline().split()))
 
-            players = sorted(players, key = lambda x: x[2], reverse=True)
-            players = sorted(players, key = lambda x: x[1])
+            players = sorted(players, key = lambda x: (x[1], x[2]), reverse=True)
 
-            print "DBG, players",players
+            assert len(players) == N, 'read wrong number of players!'
 
-        result = 'fixen!'
-        print "Case #%d: %s" % (case, result)
+            team1, team2 = [], []
+            for number, player in enumerate(players, start=1):
+                if number % 2:
+                    # odd
+                    team1.append((number, 0, player[0]))
+                else:
+                    team2.append((number, 0, player[0]))
+
+            playing = [team1[:P], team2[:P]]
+            bench = [team1[P:], team2[P:]]
+
+            for minute in range(M):
+                for team in range(2):
+                    #print "M, team", M, team
+                    #print playing[team]
+                    #print bench[team]
+                    # add minute for everyone on the field
+                    playing[team] = [addtime(player) for player in playing[team]]
+                    # subs
+                    if bench[team]:
+                        subout = sorted(playing[team], key=lambda x: (x[1], x[0]), reverse=True)[0]
+                        playing[team].remove(subout)
+                        subin = sorted(bench[team], key=lambda x: (x[1], x[0]))[0]
+                        #print "DBG: in, out: ", subin, subout
+                        bench[team].remove(subin)
+                        playing[team].append(subin)
+                        bench[team].append(subout)
+
+            result = []
+            for team in range(2):
+                for player in playing[team]:
+                    result.append(player[2]+' ')
+            print "Case #%d: %s" % (case, ''.join(sorted(result)))
