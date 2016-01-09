68a34267b7ca5debb027890dfc372e219bf5f850
tomkooij <tomkooij@tomkooij.nl>
2016-01-09 11:20:48 +0100

    fix i,j = j,i in naive solution

--- 2016/boomerang.py
+++ 2016/boomerang.py
@@ -18,7 +18,7 @@ def distance(star1, star2):
 
 
 if __name__ == '__main__':
-    with open(TESTTOM) as f:
+    with open(TESTCASE) as f:
         T = int(f.readline())
 
         for case in range(1,T+1):
@@ -64,16 +64,13 @@ if __name__ == '__main__':
             if CHECKNAIVE:
                 result_naive = 0
                 for star in stars:
-                    seen = []
                     for i in range(len(stars)):
                         if stars[i] == star: continue
-                        for j in range(len(stars)):
+                        for j in range(i, len(stars)):
                             if i==j: continue
-                            if (j,i) not in seen:
-                                seen.append((i,j))
-                                if stars[j] == star: continue
-                                if distance(star, stars[i]) == distance(star, stars[j]):
-                                    result_naive += 1
+                            if stars[j] == star: continue
+                            if distance(star, stars[i]) == distance(star, stars[j]):
+                                result_naive += 1
                 if result != result_naive:
                     print result_naive, result
                 assert result_naive == result, 'solution != naive solution'
