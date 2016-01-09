7c9011d0057eeb1323d4b4c30d5e86ae274f201d
tomkooij <tomkooij@tomkooij.nl>
2016-01-09 10:36:44 +0100

    iterate over unique distances between stars

--- 2016/boomerang.py
+++ 2016/boomerang.py
@@ -3,19 +3,18 @@
 # 50 testcases (-> 1 sec testcase)
 #  1000 stars per testcase... Naive permutation solution >> 1 min/testcase
 
-#from itertools import permutations
-#from collections import defaultdict
+from itertools import groupby
 from math import sqrt
 from random import randint
-from utils import memo, logger
 
 TESTCASE = 'input/boomerang_constellations_example_input.txt'
 INPUT = 'input/boomerang_constellations.txt'
+CHECKNAIVE = 1      # compare solution against SLOW naive solution
 
-#@logger
-@memo
+
 def distance(star1, star2):
     return sqrt((star2[0]-star1[0])**2 + (star2[1]-star1[1])**2)
+
 
 if __name__ == '__main__':
     with open(TESTCASE) as f:
@@ -23,19 +22,60 @@ if __name__ == '__main__':
 
         for case in range(1,T+1):
             N = int(f.readline())
-            field = [tuple(map(int,f.readline().rstrip('\n').split())) for line in range(N)]
+            stars = [tuple(map(int,f.readline().rstrip('\n').split())) for line in range(N)]
             #N = 10
             #field = [(randint(-10000,10000), randint(-10000,10000)) for line in range(N)]
             # check for duplicate stars in field?
 
-            result = 0
-            for star in field:
-                for i in range(len(field)):
-                    if field[i] == star: continue
-                    for j in range(len(field)):
-                        if i==j: continue
-                        if field[j] == star: continue
-                        if distance(star, field[i]) == distance(star, field[j]):
-                            result += 1
+            # get all distances
+            d = {}
+            for i in range(len(stars)):
+                for j in range(len(stars)):
+                    if i==j: continue
+                    if (j,i) in d: continue
+                    d[(i,j)] = distance(stars[i], stars[j])
 
-            print "Case #%d: %d" % (case, result/2)
+            # get all unique distances
+            unique_d = set(d.values())
+
+            result = 0
+            for dist in unique_d:
+                #print "search: ", dist
+                l = []   # list of star indexes
+                for (x,y), d1 in d.iteritems():
+                    if d1 == dist:
+                        #print (x,y)
+                        l.append(x)
+                        l.append(y)
+                l = sorted(l)
+                # count frequencies of star indexes
+                frequencies = [len(list(group)) for key, group in groupby(l)]
+                #print frequencies
+
+                # count the number of boomerang constellations:
+                # f == 1 : 0
+                # f == 2 : 1
+                # f == 3 : 3
+                # f == 4 : 4
+                # etc
+                for freq in frequencies:
+                    if freq == 2:
+                        result += freq - 1
+                    elif freq > 1:
+                        result +=freq
+            #
+            # extremely slow (complex) naive solution
+            #
+            if CHECKNAIVE:
+                result_naive = 0
+                for star in stars:
+                    for i in range(len(stars)):
+                        if stars[i] == star: continue
+                        for j in range(len(stars)):
+                            if i==j: continue
+                            if stars[j] == star: continue
+                            if distance(star, stars[i]) == distance(star, stars[j]):
+                                result_naive += 1
+                assert result_naive/2 == result, 'solution != naive solution'
+
+            print "Case #%d: %d" % (case, result)
