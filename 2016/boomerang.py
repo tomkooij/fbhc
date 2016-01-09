5df3179cf1d67519b7ad217ed4d4e0a64c2f2e3f
tomkooij <tomkooij@tomkooij.nl>
2016-01-09 13:21:41 +0100

    faster defaultdict implementation. Generate random startfield

--- 2016/boomerang.py
+++ 2016/boomerang.py
@@ -4,50 +4,55 @@
 #  1000 stars per testcase... Naive permutation solution >> 1 min/testcase
 
 from itertools import groupby
+from collections import defaultdict
 from math import sqrt
 from random import randint
 
 TESTCASE = 'input/boomerang_constellations_example_input.txt'
 TESTTOM = 'input/tom_boomerang.txt'
 INPUT = 'input/boomerang_constellations.txt'
-CHECKNAIVE = 1      # compare solution against SLOW naive solution
-
+CHECKNAIVE = 1        # compare solution against SLOW naive solution
+RANDOM_STARFIELD = 1  # generate a random starfield
 
 def distance(star1, star2):
     return sqrt((star2[0]-star1[0])**2 + (star2[1]-star1[1])**2)
 
 
 if __name__ == '__main__':
-    with open(TESTTOM) as f:
+    with open(TESTCASE) as f:
         T = int(f.readline())
 
         for case in range(1,T+1):
             N = int(f.readline())
             stars = [tuple(map(int,f.readline().rstrip('\n').split())) for line in range(N)]
-            #N = 10
-            #field = [(randint(-10000,10000), randint(-10000,10000)) for line in range(N)]
-            # check for duplicate stars in field?
 
-            # get all distances
-            d = {}
+            if RANDOM_STARFIELD:
+                N = 15
+                stars = [(i,j) for i in range(N) for j in range(N)]
+                for _ in range(randint(0,(N-1)*(N-1))):
+                    stars.pop(randint(0,len(stars)-1))
+                print "%d stars left in starfield" % (len(stars))
+
+            # create a dict with all distances. Values = list of star indexes
+            d = defaultdict(list)
             for i in range(len(stars)):
                 for j in range(i, len(stars)):
                     if i==j: continue
-                    d[(i,j)] = distance(stars[i], stars[j])
-
-            # get all unique distances
-            unique_d = set(d.values())
+                    d[distance(stars[i], stars[j])].append((i,j))
 
             result = 0
-            for dist in unique_d:
-                print "search d =", dist
-                l = []   # list of star indexes
-                for (x,y), d1 in d.iteritems():
-                    if d1 == dist:
-                        #print (x,y)
-                        l.append(x)
-                        l.append(y)
+            for dist, index_list in d.items():
+                #print "search d =", dist
+                if len(index_list) == 1:
+                    continue
+
+                # create a sorted list of all indexes
+                l = []
+                for x,y in index_list:
+                    l.append(x)
+                    l.append(y)
                 l = sorted(l)
+
                 # count frequencies of star indexes
                 frequencies = [len(list(group)) for key, group in groupby(l)]
                 #print frequencies
@@ -61,6 +66,7 @@ if __name__ == '__main__':
             # extremely slow (complex) naive solution
             #
             if CHECKNAIVE:
+                print "computing naive solution (SLOW!)"
                 result_naive = 0
                 for star in stars:
                     for i in range(len(stars)):
